b0883b3ba4f0d300b65d8a917ed1bd0befa2a7a7
Wil Faver <wilfa4243@hotmail.com>
2020-04-19 11:07:52 -0500

    Creacion rutas y pagina item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { ItemComponent } from './components/item/item.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
